981eb295cc99bfb38757b259c6a96c4f9aae40d9
george <datameshprojects@gmail.com>
2021-05-06 12:29:24 +0100

    update index file

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Microsoft Learn 2' });
+  res.render('index', { title: 'Microsoft Learn 3' });
 });
 
 module.exports = router;
